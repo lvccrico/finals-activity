34473c4aeed9c35ba3ac81a9f221b51767473b26
Rico Custodio <rico.custodio@student.laverdad.edu.ph>
2017-10-26 23:00:50 +0800

    add finals shopping cart

---
+++ resources/views/thankyou.blade.php
@@ -0,0 +1,14 @@
+@extends('master')
+
+@section('extra-css')
+    <link href="{{ asset('css/checkout.css') }}" rel="stylesheet">
+@endsection
+
+@section('content')
+
+
+    <div class="container wrapper">
+        Thank your order has been placed!
+    </div> <!-- end container -->
+
+@endsection
